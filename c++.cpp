584bb23102df6badc41c7377dd90dc4294d2236e
team9-hcmus <75234933+team9-hcmus@users.noreply.github.com>
2020-12-01 21:26:42 +0700

    Create c++.cpp

---
+++ c++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float h, s,sa;
+	cout << "enter the working hours of a month" << endl;
+	cin >> h;
+	cout << "Salary amount/hour" << endl;
+	cin >> s;
+	sa = h * s;
+	cout << "your salary" << sa;
+	return(0);
+
+
+}
